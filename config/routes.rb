e611cff79d677087c8a50505d48f32d3fd3d332e
t-yonezawa <blue_diamond_no1@yahoo.co.jp>
2019-11-25 13:45:19 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
   
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:show, :new, :create, :destroy]
   resources :tasks, except: [:index]
 end
